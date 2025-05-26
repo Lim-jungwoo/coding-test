2faaa6c87c974e4d033051da07f0ceeb32738b0d
LimJungWoo <presidentljw@naver.com>
2025-05-26 15:35:36 +0900

    solve: Boj 11053 가장 긴 증가하는 부분 수열 문제 풀이

--- src/main/java/problems/boj/Boj11053.java
+++ src/main/java/problems/boj/Boj11053.java
@@ -1,4 +1,39 @@
 package problems.boj;
 
+import java.io.*;
+import java.util.*;
+
 public class Boj11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] A = new int[n];
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 입력
+        for (int i = 0; i < n; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (A[i] < A[j])
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        bw.write(Integer.toString(result));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
 }
