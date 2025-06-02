8996df519a57fb97c095169970a52ead7f1a041a
LimJungWoo <presidentljw@naver.com>
2025-06-02 13:11:58 +0900

    solve: Boj 2740 문제 풀이

---
+++ src/main/java/problems/boj/Boj2740.java
@@ -0,0 +1,55 @@
+package problems.boj;
+
+import java.util.*;
+import java.io.*;
+
+public class Boj2740 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[][] B = new int[M][K];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] answer = new int[N][K];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                for (int k = 0; k < M; k++) {
+                    answer[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                bw.write(Integer.toString(answer[i][j]));
+                if (j != K - 1) bw.write(' ');
+            }
+            if (i != N - 1) bw.write('\n');
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
