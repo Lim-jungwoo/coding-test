71e1811bd1fc19b5de97e001ff19912727a1cd64
LimJungWoo <presidentljw@naver.com>
2025-07-09 21:31:38 +0900

    solve: Boj 2557 문제 풀이

---
+++ src/main/java/problems/boj/Boj2557.java
@@ -0,0 +1,15 @@
+package problems.boj;
+
+import java.io.*;
+import java.util.*;
+
+public class Boj2557 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+
+        bw.flush();
+        bw.close();
+    }
+}
